26c1fe2049205aed5780c8ac0bce43bcfe0dd845
frontalex <nitrolex@gmail.com>
2021-12-09 17:09:48 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/routes/api/_auth.js
@@ -0,0 +1,23 @@
+import {getServerToken, validateSitePassword} from '../../supabase/admin'
+
+export async function authorizeRequest(req, callback) {
+  const { headers, query } = req
+  const password = query.get('password')
+
+  if (password) {
+    const valid = await validateSitePassword(req.params.site, password)
+    return valid ? callback() : {
+      body: null
+    }
+  }
+
+  if (!headers.authorization) return { body: 'Must authorize request' }
+  
+  const token = headers.authorization.replace('Basic ', '')
+  const storedToken = await getServerToken()
+
+  if (token === storedToken) return callback()
+  else return {
+    body: null
+  }
+}
